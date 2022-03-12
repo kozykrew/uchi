afdd3014ad99083fc31ccc52d2051bf734e4c06b
ari-apigo <ari.apigo125@gmail.com>
2022-03-11 21:09:55 -0800

    try including design tokens in globals.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/design_tokens.css'
+// import '../styles/design_tokens.css'
 import '../styles/globals.css'
 
 import { useEffect } from 'react'
