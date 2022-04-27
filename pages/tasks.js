a10d84dd1f0333d974d9a806870f12d44f35ae62
ari-apigo <ari.apigo125@gmail.com>
2022-04-26 19:39:56 -0700

    remove tab bar from tasks page

--- pages/tasks.js
+++ pages/tasks.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout from '../components/layout.js'
 import {PageHeader} from '../components/headers.js'
+import {TaskList} from '../components/taskList.js'
 import {TabBar} from '../components/tabBar.js'
 
 const tasksByStatus = [[{id: 1, title:"Fertilize lawn", difficulty:"Easy", description:"Feed lawn with nutrients"},
@@ -10,6 +11,23 @@ const tasksByStatus = [[{id: 1, title:"Fertilize lawn", difficulty:"Easy", descr
                 [{title:"Fertilize lawn", difficulty:"Easy", description:"Feed lawn with nutrients"},
                 {title:"Fertilize lawn", difficulty:"Easy", description:"Feed lawn with nutrients"},
                 {title:"Fertilize lawn", difficulty:"Easy", description:"Feed lawn with nutrients"}]];
+
+// export default function Tasks() {
+//   return (
+//     <div>
+//       <Head>
+//         <title>UCHI | Tasks</title>
+//         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+//       </Head>
+//       <Layout>
+//         <div className="pageContent">
+//           <PageHeader iconpath="/icons/todo_gradient.png" headertext={"Tasks"} />
+//           <TabBar type="tasks" tabs={["In Progress", "Upcoming", "Completed"]} tabContent={tasksByStatus} />
+//         </div>
+//       </Layout>
+//     </div>
+//   )
+// }
 
 export default function Tasks() {
   return (
@@ -21,7 +39,7 @@ export default function Tasks() {
       <Layout>
         <div className="pageContent">
           <PageHeader iconpath="/icons/todo_gradient.png" headertext={"Tasks"} />
-          <TabBar type="tasks" tabs={["In Progress", "Upcoming", "Completed"]} tabContent={tasksByStatus} />
+          <TaskList dashboard={false} tasks={tasksByStatus[0]} />
         </div>
       </Layout>
     </div>
