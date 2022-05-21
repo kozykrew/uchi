34537810cb1a56b1fab09aefaadfff63fce4932e
ari-apigo <ari.apigo125@gmail.com>
2022-05-21 12:33:16 -0700

    lift useEffect in tasks

--- pages/tasks.js
+++ pages/tasks.js
@@ -37,15 +37,12 @@ const tasksByStatus = [[{id: 0, title:"Wash roof", difficulty:"Average", time:"3
 
 export default function Tasks() {
   const contextValue = useContext(AppContext);
+
+  const [completedTasks, setCompletedTasks] = useState([])
+  const [notTasks, setNotTasks] = useState([])
   var user;
 
-
-
-  if (contextValue.state.loggedIn) {
-    const user = supabase.auth.user()
-
-    const [completedTasks, setCompletedTasks] = useState([])
-    useEffect(() => {
+  useEffect(() => {
     if (contextValue.state.loggedIn) {
       fetchCompletedTasks()
     }
@@ -60,8 +57,7 @@ export default function Tasks() {
     else setCompletedTasks(completedTasks)
   }
 
-    const [notTasks, setNotTasks] = useState([])
-    useEffect(() => {
+  useEffect(() => {
     if (contextValue.state.loggedIn) {
       fetchNotTasks()
     }
