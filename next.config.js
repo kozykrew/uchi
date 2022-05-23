2023ba05868bdcefda2de6960b2039e644a5d592
ari-apigo <ari.apigo125@gmail.com>
2022-05-22 19:09:26 -0700

    enable CORS

--- next.config.js
+++ next.config.js
@@ -7,8 +7,22 @@ const nextConfig = {
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     })
-
     return config
+  },
+  // Enable CORS in a Next.js app: https://vercel.com/support/articles/how-to-enable-cors
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 }
 
