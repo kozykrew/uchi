1e28b738e0e2e08677aa7d54e20e8dc096ae67ee
ari-apigo <ari.apigo125@gmail.com>
2022-05-22 20:12:04 -0700

    supabase headers

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,4 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const options = {
+  headers: { 'Access-Control-Allow-Origin': '*' }
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, options)
